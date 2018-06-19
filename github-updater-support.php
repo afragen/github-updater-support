f82ac86e646401e9afd88010f19289d860cb38b6
Andy Fragen <andy@thefragens.com>
2018-06-19 12:02:30 -0700

    initial pass

---
+++ github-updater-support.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       GitHub Updater Support
+ * Plugin URI:        https://github.com/afragen/github-updater-support
+ * Description:       A support/troubleshooting plugin for use with GitHub Updater.
+ * Version:           0.0.1
+ * Author:            Andy Fragen
+ * License:           MIT
+ * Network:           true
+ * GitHub Plugin URI: https://github.com/afragen/github-updater-support
+ * Requires WP:       4.4
+ * Requires PHP:      5.6
+ */
+
+add_filter( 'github_updater_disable_wpcron', '__return_true' );
