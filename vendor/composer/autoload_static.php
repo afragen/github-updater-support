d23cca382f9bed3de09349ee0815911f358028c7
Andy Fragen <andy@thefragens.com>
2018-09-08 19:15:16 -0700

    composer update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,25 +11,9 @@ class ComposerStaticInitd1d660edd05c760138dca4d91702412e
         '35bd6a751b552e7a94ae27bc4c52b33a' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer.php',
     );
 
-    public static $prefixLengthsPsr4 = array (
-        'C' => 
-        array (
-            'Composer\\Installers\\' => 20,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'Composer\\Installers\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
-        ),
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitd1d660edd05c760138dca4d91702412e::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitd1d660edd05c760138dca4d91702412e::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
